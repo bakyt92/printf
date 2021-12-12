0accdaea120e23d3277d45ed2a7c6d31a867fbb0
ufitzhug <ufitzhug@ge-f4.kzn.21-school.ru>
2021-12-12 16:58:18 +0300

    12122021_03

--- ft_printf.h
+++ ft_printf.h
@@ -33,8 +33,8 @@ void	ft_putchar(char str, t_Data *p);
 void	ft_digit(t_Data *p);
 void	ft_unsigned_int(t_Data *p);
 void	ft_hexadecimal_lower(t_Data *p);
-void	ft_convert(unsigned long long int pointer, unsigned int base, t_Data
-			*p, char *alphabet);
+void	ft_convert(unsigned long long int pointer, unsigned int base,
+			t_Data *p, char *alphabet);
 void	ft_hexadecimal_upper(t_Data *p);
 
 #endif
